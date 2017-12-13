acf2dfef71e425e41ae4b6be0875e3711dd2078a
Hyun Bin Lee <lee559@illinois.edu>
2017-12-12 19:50:16 -0600

    modify sql

--- server/db/bf3cbe8b-0007-44e1-aabc-328efdd4af36.sql
+++ server/db/bf3cbe8b-0007-44e1-aabc-328efdd4af36.sql
@@ -1,31 +1,77 @@
-CREATE TABLE IF NOT EXISTS data (
-device_name VARCHAR(18),
-device_location VARCHAR(7),
-requester_public key VARCHAR(3),
-grantedDataRequest_ID INT,
-device_data size INT,
-pendingDataRequest_requester ID INT,
-grantedDataRequest_access start date VARCHAR(9),
-device_ID INT,
-pendingDataRequest_ID INT,
-data source_name VARCHAR(17),
-grantedDataRequest_deviceSummary ID INT,
-pendingDataRequest_access start date VARCHAR(9),
-device_type VARCHAR(11),
-pendingDataRequest_access end date VARCHAR(9),
-grantedDataRequest_access end date VARCHAR(9),
-requester_ID INT,
-deviceSummary_device ID INT,
-deviceSummary_access duration VARCHAR(24),
-device_src_ID INT,
-deviceSummary_ID INT,
-pendingDataRequest_deviceSummary ID INT,
-requester_name VARCHAR(17),
-grantedDataRequest_requester ID INT,
-data source_src_ID INT
+CREATE TABLE IF NOT EXISTS device (
+ID INT,
+data size INT,
+location VARCHAR(45),
+name VARCHAR(45),
+src_ID INT,
+type VARCHAR(45)
 );
 
-INSERT INTO data VALUES
-('Living room camera','Home','ABR',0,15000,1,'12/5/2017',0,3,'Illinois Security',0,'12/5/2017','Camera','12/5/2017','12/5/2017',0,0,'access to realtime data',0,0,2,'Anti-intruder app',0,0),
-('Phone location','varying','BLA',1,120,1,'12/5/2017',1,3,'JSM',1,'12/5/2017','GPS','12/5/2017','12/5/2017',1,0,'full access',0,1,2,'Google Photos',1,1),
-('Home thermostat','Home','HAH',2,25,1,'12/5/2017',2,3,NULL,2,'12/5/2017','temperature','12/5/2017','12/5/2017',2,2,'access to hourly average',1,2,2,'Smarthome app',2,NULL);
+INSERT INTO device VALUES
+(0,15000,'Home','Living room camera',0,'Camera'),
+(1,120,'varying','Phone location',0,'GPS'),
+(2,25,'Home','Home thermostat',1,'temperature');
+
+CREATE TABLE IF NOT EXISTS deviceSummary (
+ID INT,
+access duration VARCHAR(45),
+device ID INT
+);
+
+INSERT INTO deviceSummary VALUES
+(0,'access to realtime data',0),
+(1,'full access',0),
+(2,'access to hourly average',2);
+
+CREATE TABLE IF NOT EXISTS pendingDataRequest (
+ID INT,
+access end date VARCHAR(20),
+access start date VARCHAR(20),
+deviceSummary ID INT,
+requester ID INT
+);
+
+INSERT INTO pendingDataRequest VALUES
+(3,'12/5/2017','12/5/2017',2,1);
+
+CREATE TABLE IF NOT EXISTS grantedDataRequest (
+ID INT,
+access end date VARCHAR(20),
+access start date VARCHAR(20),
+deviceSummary ID INT,
+requester ID INT
+);
+
+INSERT INTO grantedDataRequest VALUES
+(0,'12/5/2017','12/5/2017',0,0),
+(1,'12/5/2017','12/5/2017',1,1),
+(2,'12/5/2017','12/5/2017',2,2);
+
+
+CREATE TABLE IF NOT EXISTS deniedDataRequest (
+ID INT,
+access end date VARCHAR(20),
+access start date VARCHAR(20),
+deviceSummary ID INT,
+requester ID INT
+);
+
+CREATE TABLE IF NOT EXISTS requester (
+ID INT,
+name VARCHAR(45),
+public key VARCHAR(5000)
+);
+
+INSERT INTO requester VALUES
+(0,'Anti-intruder app','ABR'),
+(1,'Google Photos','BLA'),
+(2,'Smarthome app','HAH');
+
+CREATE TABLE IF NOT EXISTS data_source (
+name VARCHAR(40),
+src_ID INT
+);
+
+INSERT INTO data_source VALUES
+('Illinois Security',0),
+('JSM',1);
